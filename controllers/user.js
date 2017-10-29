951dff7c4ec70301e8a218939d6788cca8dfb3ad
Angelini <lllouis@yahoo.com>
2017-10-29 01:58:53 +0000

    Change the way authenticate is called

--- controllers/user.js
+++ controllers/user.js
@@ -4,14 +4,16 @@ var models = require('../models');
 var strategy = require('../config/strategy')
 
 module.exports.controller = function( app, passport, passportJWT, ExtractJwt, JwtStrategy, jwt ) {
-    var jwtOptions = {}
-    jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    jwtOptions.secretOrKey = '34543534543';
+    
     console.log('user');
 
-    app.get('/api/user', passport.authenticate( 'jwt', { session: false }), function( req, res ) {
-        res.json('Reached')
+    app.get('/api/user', function( req, res ) {
+        passport.authenticate( 'jwt', { session: false }), function( req, res ) {
+                res.json('Reached')
+            }( req, res)
     })
+
+   
     
     app.get('/user', function( req, res ) {
         models.User.findAll().then( function( users ) {
